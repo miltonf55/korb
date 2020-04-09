ca69d0750c63673595732f146df52b62344dba2e
Milton Ocampo <miltonf55@hotmail.com>
2020-04-08 21:51:03 -0500

    Prueba de conección a mysql

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql=require('mysql');
 var app = express();
 const http = require("http").Server(app);
 
@@ -6,6 +7,22 @@ app.set("port", (process.env.PORT || 5555));
 http.listen(app.get("port"), function() {
 	console.log("Servidor en el puerto", app.get("port"));
 });
+
+var con= mysql.createConnection("mysql://ba36d017d9e475:2e5db296@us-cdbr-iron-east-01.cleardb.net/heroku_7a19aa0bce95bcf?reconnect=true");
+    /*{
+	host:'us-cdbr-iron-east-01.cleardb.net',
+	user:'ba36d017d9e475',
+	password:'2e5db296',
+	database:'heroku_7a19aa0bce95bcf'
+});*/
+con.connect(function(err) {
+    if (err) {
+      console.error('Error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('Connected as id ' + connection.threadId);
+  });
 
 app.use(express.static("public"));
 
