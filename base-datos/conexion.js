8d2b57d243a9c6eaa1be2f88e57b796fbabaafff
Milton Ocampo <miltonf55@hotmail.com>
2020-06-09 16:27:10 -0500

    Correccion de base

--- base-datos/conexion.js
+++ base-datos/conexion.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql");
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "n0m3l0",
-    database: "korbdb",
-    port: 3306
-});
 
+//Configuración de la conexión a la db hosteada
+var confCon={
+    connectionLimit:10,
+	host:'us-cdbr-iron-east-01.cleardb.net',
+	user:'ba36d017d9e475',
+	password:'2e5db296',
+	database:'heroku_7a19aa0bce95bcf'
+};
+var pool= mysql.createPool(confCon);
+pool.on('acquire', function (connection) {
+    console.log('Connection %d acquired', connection.threadId);
+});
+pool.on('connection', function (connection) {
+    connection.query('SET SESSION auto_increment_increment=1')
+});
 const {
     cifrar,
     decifrar
